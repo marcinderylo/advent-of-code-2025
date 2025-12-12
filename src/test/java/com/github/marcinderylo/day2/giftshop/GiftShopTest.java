225927f4a0bbec67bb4622d482b4e2d89515056b
Marcin Deryło <marcin.derylo@ocado.com>
2025-12-12 16:08:54 +0100

    Day 2, part 2 task

--- src/test/java/com/github/marcinderylo/day2/giftshop/GiftShopTest.java
+++ src/test/java/com/github/marcinderylo/day2/giftshop/GiftShopTest.java
@@ -7,6 +7,7 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Arrays;
+import java.util.function.LongPredicate;
 import java.util.stream.LongStream;
 import java.util.stream.Stream;
 
@@ -46,52 +47,70 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
  * <p>
  * What do you get if you add up all of the invalid IDs?
  * </p>
+ *
+ * --- Part Two ---
+ * The clerk quickly discovers that there are still invalid IDs in the ranges in your list. Maybe the young Elf was doing other silly patterns as well?
+ * Now, an ID is invalid if it is made only of some sequence of digits repeated at least twice. So, 12341234 (1234 two times), 123123123 (123 three times), 1212121212 (12 five times), and 1111111 (1 seven times) are all invalid IDs.
+ * From the same example as before:
+ * 11-22 still has two invalid IDs, 11 and 22.
+ * 95-115 now has two invalid IDs, 99 and 111.
+ * 998-1012 now has two invalid IDs, 999 and 1010.
+ * 1188511880-1188511890 still has one invalid ID, 1188511885.
+ * 222220-222224 still has one invalid ID, 222222.
+ * 1698522-1698528 still contains no invalid IDs.
+ * 446443-446449 still has one invalid ID, 446446.
+ * 38593856-38593862 still has one invalid ID, 38593859.
+ * 565653-565659 now has one invalid ID, 565656.
+ * 824824821-824824827 now has one invalid ID, 824824824.
+ * 2121212118-2121212124 now has one invalid ID, 2121212121.
+ * Adding up all the invalid IDs in this example produces 4174379265.
+ * What do you get if you add up all of the invalid IDs using these new rules?
  */
 public class GiftShopTest {
     @Test
     void twoInvalidIdsInRange_11_22() {
-        assertArrayEquals(new long[]{11, 22}, findInvalidIdsInRange(11, 22));
+        assertArrayEquals(new long[]{11, 22}, findInvalidIdsInRange(11, 22, GiftShopTest::doubledChars));
     }
 
     @Test
     void singleInvalidIdInRange_95_115() {
-        assertArrayEquals(new long[]{99}, findInvalidIdsInRange(95, 115));
+        assertArrayEquals(new long[]{99}, findInvalidIdsInRange(95, 115, GiftShopTest::doubledChars));
     }
 
     @Test
     void range_998_1012() {
-        assertArrayEquals(new long[]{1010}, findInvalidIdsInRange(998, 1012));
+        assertArrayEquals(new long[]{1010}, findInvalidIdsInRange(998, 1012, GiftShopTest::doubledChars));
     }
 
     @Test
     void sumOfInvalidIdsInExample() {
         String input = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
-        assertEquals(1227775554, solutionFor(individualRanges(input)));
+        assertEquals(1227775554, solutionFor(individualRanges(input), GiftShopTest::doubledChars));
     }
 
     public static void main(String[] args) throws IOException {
-        System.out.println(solutionFor(inputRanges()));
+        System.out.println(solutionFor(inputRanges(), GiftShopTest::doubledChars));
     }
 
-    private static long solutionFor(Stream<Pair> ranges) {
+    private static long solutionFor(Stream<Pair> ranges, LongPredicate invalidIdPredicate) {
         return ranges
-                .flatMapToLong(GiftShopTest::findInvalidIdsInRange)
+                .flatMapToLong(pair -> findInvalidIdsInRange(pair, invalidIdPredicate))
                 .sum();
 
     }
 
-    private static long[] findInvalidIdsInRange(int min, int max) {
-        return findInvalidIdsInRange(new Pair(min, max)).toArray();
+    private static long[] findInvalidIdsInRange(long min, long max, LongPredicate invalidIdPredicate) {
+        return findInvalidIdsInRange(new Pair(min, max), invalidIdPredicate).toArray();
     }
 
-    private static LongStream findInvalidIdsInRange(Pair pair) {
+    private static LongStream findInvalidIdsInRange(Pair pair, LongPredicate invalidIdPredicate) {
         return LongStream.rangeClosed(pair.min(), pair.max())
-                .filter(GiftShopTest::isInvalidId);
+                .filter(invalidIdPredicate);
     }
 
 
-    private static boolean isInvalidId(long id) {
+    private static boolean doubledChars(long id) {
         String idStr = Long.toString(id);
         if (idStr.length() % 2 == 1) {
             return false;
